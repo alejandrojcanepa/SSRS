e5e0ade873dcc47a03ce7260f3257049dbfbc438
Alec <alejandro.canepa@nybblegroup.com>
2017-12-01 12:46:10 -0800

    Finished Third Party Authorization Report

--- appSSRS.Reports/ThirdPartyAuthorizationReport.vb
+++ appSSRS.Reports/ThirdPartyAuthorizationReport.vb
@@ -1,32 +1,7 @@
-﻿''' <summary>
-''' Clase para manejar la funcionalidad del reporte Third Party Authorization
-''' </summary>
-Public Class ThirdPartyAuthorizationReport
+﻿Public Class ThirdPartyAuthorizationReport
     Inherits BaseReport
 
-    ''' <summary>
-    ''' Obtiene un número que determina por que campo agrupar
-    ''' </summary>
-    ''' <param name="date_of_death">Corresponde al campo opportunity_ext.date_of_death</param>
-    ''' <param name="policy_owner">Corresponde al campo policies.owner</param>
-    ''' <param name="programNumeral">Corresponde al campo view_3rd_party_authorization.program #</param>
-    ''' <param name="confirmed_lapse_date">Corresponde al campo prem_reqs_new.confirmed_lapse_date</param>
-    ''' <param name="ltr_22_month_response">Corresponde al campo loans.ltr_22_month_response</param>
-    ''' <param name="note_purchaser">Corresponde al campo loans.note_purchaser</param>
-    ''' <param name="endorsee">Corresponde al campo policies.endorsee</param>
-    ''' <param name="purchaser">Corresponde al campo view_best_purchaser_offer_accepted.purchaser</param>
-    ''' <param name="VBPOAProvider">Corresponde al campo view_best_purchaser_offer_accepted.provider</param>
-    ''' <param name="kbc_decision">Corresponde al campo view_premium_latest_by_source_date.kbc_decision</param>
-    ''' <param name="status">Corresponde al campo view_policy_lonsdale_status_latest.status</param>
-    ''' <param name="payoff_date_service">Corresponde al campo loans.payoff_date_service</param>
-    ''' <param name="program">Corresponde al campo view_policy_consolidated.program</param>
-    ''' <param name="strike_price_paid">Corresponde al campo option_exercise_notice.strike_price_paid</param>
-    ''' <param name="VPCProvider">Corresponde al campo View_Policy_Consolidated.provider</param>
-    ''' <param name="original_strike_price_check">Corresponde al campo option_exercise_notice.original_strike_price_check</param>
-    ''' <param name="adj_strike_price_check">Corresponde al campo option_exercise_notice.adj_strike_price_check</param>
-    ''' <returns>Un número entre 100 y 4 que determina por que campo agrupar</returns>
     Function GroupLabel(ByVal date_of_death As Nullable(Of Date), ByVal policy_owner As String, ByVal programNumeral As String, ByVal confirmed_lapse_date As Nullable(Of Date), ByVal ltr_22_month_response As String, ByVal note_purchaser As String, ByVal endorsee As String, ByVal purchaser As String, ByVal VBPOAProvider As String, ByVal kbc_decision As String, ByVal status As String, ByVal payoff_date_service As Nullable(Of Date), ByVal program As String, ByVal strike_price_paid As Nullable(Of Date), ByVal VPCProvider As String, ByVal original_strike_price_check As String, ByVal adj_strike_price_check As String) As Decimal
-
         If Not IsNothing(date_of_death) Then
             Return 100
         ElseIf policy_owner = "EFG" Then
@@ -64,29 +39,7 @@ Public Class ThirdPartyAuthorizationReport
         End If
     End Function
 
-    ''' <summary>
-    ''' Obtiene el campo por el que se debe agrupar
-    ''' </summary>
-    ''' <param name="date_of_death">Corresponde al campo opportunity_ext.date_of_death</param>
-    ''' <param name="policy_owner">Corresponde al campo policies.owner</param>
-    ''' <param name="programNumeral">Corresponde al campo view_3rd_party_authorization.program #</param>
-    ''' <param name="confirmed_lapse_date">Corresponde al campo prem_reqs_new.confirmed_lapse_date</param>
-    ''' <param name="ltr_22_month_response">Corresponde al campo loans.ltr_22_month_response</param>
-    ''' <param name="note_purchaser">Corresponde al campo loans.note_purchaser</param>
-    ''' <param name="endorsee">Corresponde al campo policies.endorsee</param>
-    ''' <param name="purchaser">Corresponde al campo view_best_purchaser_offer_accepted.purchaser</param>
-    ''' <param name="VBPOAProvider">Corresponde al campo view_best_purchaser_offer_accepted.provider</param>
-    ''' <param name="kbc_decision">Corresponde al campo view_premium_latest_by_source_date.kbc_decision</param>
-    ''' <param name="status">Corresponde al campo view_policy_lonsdale_status_latest.status</param>
-    ''' <param name="payoff_date_service">Corresponde al campo loans.payoff_date_service</param>
-    ''' <param name="program">Corresponde al campo view_policy_consolidated.program</param>
-    ''' <param name="strike_price_paid">Corresponde al campo option_exercise_notice.strike_price_paid</param>
-    ''' <param name="VPCProvider">Corresponde al campo View_Policy_Consolidated.provider</param>
-    ''' <param name="original_strike_price_check">Corresponde al campo option_exercise_notice.original_strike_price_check</param>
-    ''' <param name="adj_strike_price_check">Corresponde al campo option_exercise_notice.adj_strike_price_check</param>
-    ''' <returns>Un string con el campo por el que se debe agrupar</returns>
     Function GroupBy(ByVal date_of_death As Nullable(Of Date), ByVal policy_owner As String, ByVal programNumeral As String, ByVal confirmed_lapse_date As Nullable(Of Date), ByVal ltr_22_month_response As String, ByVal note_purchaser As String, ByVal endorsee As String, ByVal purchaser As String, ByVal VBPOAProvider As String, ByVal kbc_decision As String, ByVal status As String, ByVal payoff_date_service As Nullable(Of Date), ByVal program As String, ByVal strike_price_paid As Nullable(Of Date), ByVal VPCProvider As String, ByVal original_strike_price_check As String, ByVal adj_strike_price_check As String) As String
-
         If GroupLabel(date_of_death, policy_owner, programNumeral, confirmed_lapse_date, ltr_22_month_response, note_purchaser, endorsee, purchaser, VBPOAProvider, kbc_decision, status, payoff_date_service, program, strike_price_paid, program, original_strike_price_check, adj_strike_price_check) = 100 Then
             Return "Deceased"
         ElseIf GroupLabel(date_of_death, policy_owner, programNumeral, confirmed_lapse_date, ltr_22_month_response, note_purchaser, endorsee, purchaser, VBPOAProvider, kbc_decision, status, payoff_date_service, program, strike_price_paid, program, original_strike_price_check, adj_strike_price_check) = 99 Then
